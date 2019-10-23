d809e779024b36e82f42e9213e88dca594a6ba24
wei <ge25zir@tum.de>
2019-10-23 21:57:23 +0200

    code2 finish

--- src/main.c
+++ src/main.c
@@ -1,8 +1,25 @@
 #include<stdio.h>
 #include "espl_lib.h"
+#include<string.h>
 void main()
 {
-	printf("Hello ESPL\n");
-	printf("This is a new print");
+	unsigned int i;
+	char* s;
+	printf("please give me a number: \n");
+	scanf("%d",i);
+	s = num_to_words(i);
+	printf("output string is %s\n",s);
+	printf("another try?\nexit with input char 'q' \n");
+	scanf("%d",i);
+	while(i!='q')
+	{
+		s = num_to_words(i);
+		printf("output string is %s\n",s);
+		printf("another try?\nexit with input char 'q' \n");
+		scanf("%d",i);
 
+	}	
+	printf("game over");
+	
+	
 }
