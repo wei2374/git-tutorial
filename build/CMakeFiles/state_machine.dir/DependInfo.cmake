b189f60951d662e5530cc5b4b357618727d503fb
wei <ge25zir@tum.de>
2019-10-25 10:24:07 +0200

    no idea

---
+++ build/CMakeFiles/state_machine.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wei/Documents/git-tutorial/src/main.c" "/home/wei/Documents/git-tutorial/build/CMakeFiles/state_machine.dir/src/main.c.o"
+  "/home/wei/Documents/git-tutorial/src/my_states.c" "/home/wei/Documents/git-tutorial/build/CMakeFiles/state_machine.dir/src/my_states.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/wei/Documents/git-tutorial/build/CMakeFiles/STATES_LIB.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
